182e9f80695f460313ca9f12622c3474d2a1be45
SMS3384 <akkuntsms8@gmail.com>
2024-12-10 10:31:47 +0500

    models, urls

--- blogapp/models.py
+++ blogapp/models.py
@@ -34,14 +34,14 @@ class Post(models.Model):
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.publish.year,
-                                                 self.publish.month,
-                                                 self.publish.day,
-                                                 self.slug])
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
 
-posts = Post.objects.filter(status='published')
-p_posts = Post.published.all()
+
 
 
 
